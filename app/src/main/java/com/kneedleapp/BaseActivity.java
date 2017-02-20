8d0bfaeebe1f3e324a6b7c99a1e57ef7bcea6ca2
Vikesh <vikesh.jhajhria@gmail.com>
2017-02-21 02:31:51 +0530

    21/02/2017

--- app/src/main/java/com/kneedleapp/BaseActivity.java
+++ app/src/main/java/com/kneedleapp/BaseActivity.java
@@ -4,6 +4,10 @@ import android.app.ActivityManager;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
@@ -75,6 +79,68 @@ public class BaseActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.img_back:
                 onBackPressed();
                 break;
+        }
+    }
+
+    protected void addFragment(@IdRes int containerViewId,
+                               @NonNull Fragment fragment,
+                               @NonNull String fragmentTag) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(containerViewId, fragment, fragmentTag)
+                .disallowAddToBackStack()
+                .commit();
+    }
+
+    protected void replaceFragment(@IdRes int containerViewId,
+                                   @NonNull Fragment fragment,
+                                   @NonNull String fragmentTag,
+                                   @Nullable String backStackStateName) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(containerViewId, fragment, fragmentTag)
+                .addToBackStack(backStackStateName)
+                .commit();
+    }
+
+    protected void showFragment(@IdRes int containerViewId,
+                                @NonNull Fragment fragment,
+                                @NonNull String fragmentTag) {
+        if (!showFragment(fragmentTag)) {
+            addFragment(containerViewId, fragment, fragmentTag);
+        }
+    }
+
+    protected boolean showFragment(@NonNull String tag) {
+        Fragment fragment = getSupportFragmentManager().findFragmentByTag(tag);
+        if (fragment != null) {
+            hideAllFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .show(fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    protected void hideFragment(@NonNull Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .hide(fragment)
+                .commit();
+    }
+
+    protected void removeFragment(@NonNull Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .remove(fragment)
+                .commit();
+    }
+
+    protected void hideAllFragment() {
+        for (Fragment fragment : getSupportFragmentManager().getFragments()) {
+            hideFragment(fragment);
         }
     }
 }
--- app/src/main/java/com/kneedleapp/MainActivity.java
+++ app/src/main/java/com/kneedleapp/MainActivity.java
@@ -3,7 +3,14 @@ package com.kneedleapp;
 import android.os.Bundle;
 import android.view.View;
 
+import com.kneedleapp.fragment.HomeFragment;
+import com.kneedleapp.fragment.ItemFragment;
+import com.kneedleapp.fragment.NotificationFragment;
+import com.kneedleapp.fragment.PostFragment;
+import com.kneedleapp.fragment.ProfileFragment;
+
 public class MainActivity extends BaseActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,21 +50,27 @@ public class MainActivity extends BaseActivity {
         findViewById(R.id.rl_notification_selected).setVisibility(View.INVISIBLE);
         findViewById(R.id.rl_profile_selected).setVisibility(View.INVISIBLE);
 
+
         switch (selectedTab) {
             case HOME:
                 findViewById(R.id.rl_home_selected).setVisibility(View.VISIBLE);
+                showFragment(R.id.main_frame, HomeFragment.newInstance("",""),"HOME_FRAGMENT");
                 break;
             case FEEd:
                 findViewById(R.id.rl_feed_selected).setVisibility(View.VISIBLE);
+                showFragment(R.id.main_frame, ItemFragment.newInstance(1),"FEED_FRAGMENT");
                 break;
             case POST:
                 findViewById(R.id.rl_post_selected).setVisibility(View.VISIBLE);
+                showFragment(R.id.main_frame, PostFragment.newInstance("",""),"POST_FRAGMENT");
                 break;
             case NOTIFICATION:
                 findViewById(R.id.rl_notification_selected).setVisibility(View.VISIBLE);
+                showFragment(R.id.main_frame, NotificationFragment.newInstance("",""),"NOTIFICATION_FRAGMENT");
                 break;
             case PROFILE:
                 findViewById(R.id.rl_profile_selected).setVisibility(View.VISIBLE);
+                showFragment(R.id.main_frame, ProfileFragment.newInstance("",""),"PROFILE_FRAGMENT");
                 break;
         }
     }
