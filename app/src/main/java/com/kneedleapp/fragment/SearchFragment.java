3f71fe4974a5222990b877fa7efb8d80a825ace1
Vikesh <vikesh.jhajhria@gmail.com>
2017-03-06 02:12:08 +0530

    06/03/2017

--- app/src/main/java/com/kneedleapp/fragment/SearchFragment.java
+++ app/src/main/java/com/kneedleapp/fragment/SearchFragment.java
@@ -4,11 +4,12 @@ package com.kneedleapp.fragment;
 import android.content.Context;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -19,41 +20,21 @@ import com.kneedleapp.utils.Utils;
 
 import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SearchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SearchFragment extends BaseFragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-    private View view;
 
     private String searchText = "";
 
-    // TODO: Rename and change types and number of parameters
+    ArrayList<String> spinnerDataList;
+    ArrayList<String> withinList;
+
     public static SearchFragment newInstance(String param1, String param2) {
         SearchFragment fragment = new SearchFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
 
@@ -61,13 +42,32 @@ public class SearchFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        view = inflater.inflate(R.layout.fragment_search, container, false);
+        final View view = inflater.inflate(R.layout.fragment_search, container, false);
         ((Spinner) view.findViewById(R.id.spinner_home)).getBackground().setColorFilter(getResources().getColor(R.color.textColorPrimary), PorterDuff.Mode.SRC_ATOP);
 
+        ((CheckBox)view.findViewById(R.id.check_near_me)).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked){
+                    view.findViewById(R.id.rl_zip).setVisibility(View.VISIBLE);
+                } else {
+                    view.findViewById(R.id.rl_zip).setVisibility(View.INVISIBLE);
+                }
+            }
+        });
         spinnerDataList = new ArrayList<>();
         spinnerDataList.add("PROFILE TYPE");
         spinnerDataList.add("PROFILE 1");
         spinnerDataList.add("PROFILE 2");
+
+        withinList = new ArrayList<>();
+        withinList.add("10 KM");
+        withinList.add("50 KM");
+        withinList.add("100 KM");
+        withinList.add("200 KM");
+        WithinSpinnerAdapter withinAdapter = new WithinSpinnerAdapter(getActivity(), R.layout.layout_spinner_item, withinList);
+        ((Spinner) view.findViewById(R.id.spinner_within)).setAdapter(withinAdapter);
+
         view.findViewById(R.id.img_search).setOnClickListener(this);
 
         SpinnerAdapter spinnerAdapter = new SpinnerAdapter(getActivity(), R.layout.layout_spinner_item, spinnerDataList);
@@ -77,7 +77,6 @@ public class SearchFragment extends BaseFragment {
         return view;
     }
 
-    ArrayList<String> spinnerDataList;
 
 
     public class SpinnerAdapter extends ArrayAdapter<String> {
@@ -106,13 +105,40 @@ public class SearchFragment extends BaseFragment {
         }
     }
 
+
+    public class WithinSpinnerAdapter extends ArrayAdapter<String> {
+
+        public WithinSpinnerAdapter(Context context, int textViewResourceId, ArrayList<String> objects) {
+            super(context, textViewResourceId, objects);
+        }
+
+        @Override
+        public View getDropDownView(int position, View convertView, ViewGroup parent) {
+            return getCustomView(position, convertView, parent);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return getCustomView(position, convertView, parent);
+        }
+
+        public View getCustomView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = getActivity().getLayoutInflater();
+            View row = inflater.inflate(R.layout.layout_spinner_item, parent, false);
+            TextView label = (TextView) row.findViewById(R.id.txt_item);
+            Utils.setTypeface(getActivity(), label, Config.CENTURY_GOTHIC_REGULAR);
+            label.setText(withinList.get(position));
+            return row;
+        }
+    }
+
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.img_search:
-                searchText = ((EditText)getView().findViewById(R.id.txt_search)).getText().toString().trim();
-                if(searchText.isEmpty()){
-                    ((EditText)getView().findViewById(R.id.txt_search)).setError("Please enter key to search.");
+                searchText = ((EditText) getView().findViewById(R.id.txt_search)).getText().toString().trim();
+                if (searchText.isEmpty()) {
+                    ((EditText) getView().findViewById(R.id.txt_search)).setError("Please enter key to search.");
                     break;
                 }
                 SearchResultFragment fragment = SearchResultFragment.newInstance();
@@ -126,6 +152,8 @@ public class SearchFragment extends BaseFragment {
 
     private void applyFonts(View view) {
         Utils.setTypeface(getActivity(), (TextView) view.findViewById(R.id.txt_search), Config.CENTURY_GOTHIC_REGULAR);
+        Utils.setTypeface(getActivity(), (TextView) view.findViewById(R.id.txt_zip), Config.CENTURY_GOTHIC_REGULAR);
+        Utils.setTypeface(getActivity(), (TextView) view.findViewById(R.id.check_near_me), Config.CENTURY_GOTHIC_REGULAR);
     }
 
 }
