5c7c7f3d721082da5fd84f5dacabd306f215a65d
vikeshjhajhria <vikesh.jhajhria@gmail.com>
2017-07-26 18:55:17 +0530

    26-07-2017

---
+++ app/src/main/java/com/kneedleapp/MessageMain.java
@@ -0,0 +1,13 @@
+package com.kneedleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MessageMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_user_list);
+    }
+}
